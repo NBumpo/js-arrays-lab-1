c232d7adb364ca7db00b9a282ac6965dae2e60d6
nathanaelchojnacki <nbumpo11@gmail.com>
2023-09-27 10:52:30 -0500

    completed exercises 1 + 2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 
 
 console.log('My app.js is being run!')
+
+const foods = [];
+console.log('Exercise 1 Result:\n', foods);
+foods.unshift("pizza", "cheeseburger")
+console.log('Exercise 2 Result:\n',  foods);

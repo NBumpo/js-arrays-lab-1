528e99a1398eca7e41f33197dfc7dd0808a84392
nathanaelchojnacki <nbumpo11@gmail.com>
2023-09-27 11:42:26 -0500

    Exercises 3 + 4
    git push origin main

--- js/app.js
+++ js/app.js
@@ -6,3 +6,9 @@ const foods = [];
 console.log('Exercise 1 Result:\n', foods);
 foods.unshift("pizza", "cheeseburger")
 console.log('Exercise 2 Result:\n',  foods);
+foods.unshift("taco")
+
+console.log('Exercise 3 Result:\n', foods);
+let favFood = foods[1]
+
+console.log('Exercise 4 Result:\n', favFood);
